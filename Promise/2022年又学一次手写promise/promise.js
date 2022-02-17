8bbb11a8b7fdab977b4cafccf73ea9401319c4a5
yangjingqi <18179671772@163.com>
2022-02-17 08:22:21 +0800

    feat: 补充.then返回的值（同步）

--- Promise/2022年又学一次手写promise/promise.js
+++ Promise/2022年又学一次手写promise/promise.js
@@ -5,7 +5,8 @@ function Promise(executor) {
     this.PromiseResult = undefined
 
     // 指定回调，就算promise内部是异步任务，等到状态改变了再来执行.then里面的回调方法
-    this.callBack = {}
+    // this.callBack = {}
+    this.callBacks = []
 
     // this指向有问题
     // let self = this
@@ -24,13 +25,23 @@ function Promise(executor) {
         if (this.PromiseStatus !== 'pending') return
         this.PromiseStatus = 'fulfilled'
         this.PromiseResult = value
-        this.callBack.onResolved(value)
+        // if (this.callBack.onResolved) {
+        //     this.callBack.onResolved(value)
+        // }
+        this.callBacks.forEach(item => {
+            item.onResolved(value)
+        })
     }
     const reject = (reason) => {
         if (this.PromiseStatus !== 'pending') return
         this.PromiseStatus = 'rejected'
         this.PromiseResult = reason
-        this.callBack.onRejected(reason)
+        // if (this.callBack.onRejected) {
+        //     this.callBack.onRejected(reason)
+        // }
+        this.callBacks.forEach(item => {
+            item.onRejected(reason)
+        })
     }
 
     // try加在执行   执行器函数前面即可
@@ -47,21 +58,57 @@ function Promise(executor) {
 
 Promise.prototype.then = function (onResolved, onRejected) {
     return new Promise((resolve, reject) => {
+        // try { 不用再try catch了  执行器函数那里的try catch已经拦截到了
         if (this.PromiseStatus === 'fulfilled') {
             // 调用回调函数
-            onResolved(this.PromiseResult)
+            const res = onResolved(this.PromiseResult)
+            if (res instanceof Promise) {
+                // return res   错误写法 ，直接返回这个promise对象
+
+                res.then(r => {
+                    resolve(r)
+                }, j => {
+                    reject(j)
+                })
+
+            } else {
+                // 如果返回的结构不是Promise的实例，
+                // 则结果的对象状态为成功  成功的结果为return的结果
+                // tips: 这里不需要手动再返回一个Promise了，外部已经返回了一个Promise对象
+                // 这里多此一举  但是为什么又不影响返回值呢？  猜测是同步执行 + 调用的外层的resolve
+                // return new Promise((r, j) => {
+                // r(res) ?为啥不用的这里的 r(res)
+                resolve(res)
+                // })
+            }
         }
 
+        // } catch (e) {
+        //     reject(e)
+        // }
+        // console.log(e);
         if (this.PromiseStatus === 'rejected') {
             //调用回调函数
-            onRejected(this.PromiseResult)
+            const res = onRejected(this.PromiseResult)
+            if (res instanceof Promise) {
+                res.then(r => {
+                    resolve(r)
+                }, j => {
+                    reject(j)
+                })
+            } else {
+                // tips: 注意，这里不是reject(res)
+                resolve(res)
+            }
         }
 
+
+
         if (this.PromiseStatus === 'pending') {
-            this.callBack = {
+            this.callBacks.push({
                 onResolved: onResolved,
                 onRejected: onRejected
-            }
+            })
         }
     })
 }
