912a07d3d1ff3f2b1eb9e907843a0a5cda63e2eb
yangjingqi <18179671776@163.com>
2022-03-30 16:19:40 +0800

    fix: 需要留意斐波那契

---
+++ 练习题/01_斐波那契.js
@@ -0,0 +1,12 @@
+// var res = function (n) {
+//     function foo(m = 0) {
+//         m = m + m + 1
+//         foo(m)
+//         if (m === n) {
+//             return m
+//         }
+//     }
+//     foo(5)
+// }
+
+// console.log(res);
