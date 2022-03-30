fc933867d07a9d2c82e181ddd39640ecff5e0e8e
yangjingqi <18179671776@163.com>
2022-03-30 19:56:36 +0800

    提交代码

--- 练习题/对象/03_a.x和b.x经典做错几次.js
+++ 练习题/对象/03_a.x和b.x经典做错几次.js
@@ -5,6 +5,7 @@ var b = a
 a.x = a = {
     n: 2
 }
+// . 的优先级比=赋值的优先级要高。所以先查询a.x 此时a和b还指向的同一个地址。然后从右往左开始赋值的。
 console.log(a);
 console.log(b);
 console.log(a.x);
--- 练习题/对象/04_对象的key.js
+++ 练习题/对象/04_对象的key.js
@@ -1,25 +1,27 @@
+// 对象的key属性只能是字符串或者是Symbol类型的
 var a = {},
     b = '123',
     c = 123
 a[b] = 'b';
 a[c] = 'c'
-
+// 这里是同名了，所以还是'c'
 console.log(a[b]);
 
-// var d = {},
-//     e = Symbol('123'),
-//     f = Symbol('123')
-// d[e] = 'b'
-// d[f] = 'c'
-// console.log(d[e]);
+var a = {},
+    b = Symbol('123'),
+    c = Symbol('123')
+a[b] = 'b'
+a[c] = 'c'
+console.log(a[b]);
 
-// var a = {},
-//     b = {
-//         key: '123'
-//     },
-//     c = {
-//         key: '456'
-//     }
-// a[b] = 'b'
-// a[c] = 'c'
-// console.log(a[b]);
+// 这里的就成了'[object Object]' 被覆盖了
+var a = {},
+    b = {
+        key: '123'
+    },
+    c = {
+        key: '456'
+    }
+a[b] = 'b'
+a[c] = 'c'
+console.log(a[b]);
