0bee73dfc883c804911f5c6a7b44ccb30cd8ef10
yangjingqi <18179671776@163.com>
2022-07-04 15:24:23 +0800

    fix: 订正一下charAt和charCodeAt的用和名称

--- 练习题/数组/07_以第二个字母来排序.js
+++ 练习题/数组/07_以第二个字母来排序.js
@@ -6,3 +6,19 @@ let a = words.sort((a, b) => {
   // 目前还有缺陷，没有第三个字母的顺序跟题目要求的顺序不对
 })
 console.log(a);
+
+// 同时还了解了 字符串的charAt()和charCodeAt()
+let b = 'abc'
+// charAt 返回指定位置的字符
+console.log(b.charAt(0));
+console.log(b.charAt());
+// 超出的返回一个空字符串
+console.log(b.charAt(4));
+
+// 返回指定位置的编码  
+console.log(b.charCodeAt(0))
+console.log(b.charCodeAt())
+// 如果传入的不是一个数值，则默认传入的是0
+console.log(b.charCodeAt('adasdasdasds'))
+// 超出部分返回NaN
+console.log(b.charCodeAt(4))
